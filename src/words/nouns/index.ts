abf051bb91bcc9605145c254fbbbd803c5a973ae
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-08-22 23:21:54 +0500

    Add three more nouns

--- src/words/nouns/index.ts
+++ src/words/nouns/index.ts
@@ -1,9 +1,15 @@
 import { Noun, PluralNoun, SingularNoun } from '../../types'
+import блядь from './блядь'
 import пизда from './пизда'
+import пиздец from './пиздец'
+import сука from './сука'
 import хуй from './хуй'
 
 const nouns: Array<SingularNoun | PluralNoun | Noun> = [
+  блядь,
   пизда,
+  пиздец,
+  сука,
   хуй
 ]
 
